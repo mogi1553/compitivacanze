8f6b0e2ad0987518d51e1aeb5fe9293e1db0f989
mogi1553 <igormogildea8@gmail.com>
2023-07-23 20:33:33 +0200

    esercizio funzioni : trovare quale dei 2 tempi è il maggiore

--- esercizio2/main.cpp
+++ esercizio2/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+using namespace std;
+
+int tempo(int ore, int minuti , int secondi){
+    int t;
+    minuti = 60*minuti;
+    ore=60*ore*60;
+    t=secondi+ore+minuti;
+    return t;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+   int ore , minuti , secondi, t, t1, ore1 , minuti1 , secondi1;
+   cin >> ore>>minuti>>secondi;
+   cin >>ore1>>minuti1>>secondi1;
+   t= tempo(ore, minuti, secondi);
+   t1=tempo(ore1, minuti1,secondi1);
+    if (t>t1){
+        cout<<"t risulta maggiore di t1";
+    }
+    else{
+       cout<<"t1 risulta maggiore di t";
+    }
+
+
     return 0;
 }
