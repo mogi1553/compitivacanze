38ab008588491f3a0c7840aa461d13ff13a2540b
mogi1553 <igormogildea8@gmail.com>
2023-07-23 19:42:58 +0200

    inizio esercizi

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+ float Lunghezza(float P) {
+    float g = 9.8, L;
+    L=((P/6.2832)*g)*((P/6.2832)*g);
+    return L;
+}
+
+int main () {
+float P;
+cin>>P;
+cout<<Lunghezza(P);
+
+return 0;
 }
